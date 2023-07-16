6dd96e128504692ef61502192289cc1ca339f1a7
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2023-07-16 16:26:33 +0200

    Update

--- Back-End/ExpressJS/05-rest-api.js
+++ Back-End/ExpressJS/05-rest-api.js
@@ -3,13 +3,125 @@ const express = require("express"),
 const app = express();
 const PORT = 3000;
 
-// Make the images directory a publicly accessible asset
-app.use(express.static(path.join(__dirname, "public", "images")));
+// Apply necessary middleware
+app.use(express.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Home page");
-});
+// Define the Post class object
+class Post {
+  id = Math.floor(Math.random() * 1000) + 1000;
+
+  constructor(title, author, content) {
+    this.title = title;
+    this.author = author;
+    this.content = content;
+  }
+}
+
+// Define the dummy posts
+let posts = [
+  new Post("1st Post", "User 1", "This is the content"),
+  new Post("2nd Post", "User 2", "This is the content"),
+  new Post("3rd Post", "User 3", "This is the content"),
+];
+
+// Define Routes for GET and POST
+app
+  .route("/posts")
+  .get((req, res, next) => {
+    res.status(200).json({
+      data: [posts],
+      message: "All posts fetched successfully!",
+    });
+  })
+  .post((req, res, next) => {
+    const { title, author, content } = req.body;
+
+    // Check if input data is complete
+    if (!title || !author || !content) {
+      return res.status(422).json({
+        error: "Input data is incomplete!",
+      });
+    }
+
+    // Create and add the new post
+    const postNew = new Post(title, author, content);
+    posts.push(postNew);
+
+    res.status(201).json({
+      data: [posts],
+      message: "A new post created successfully!",
+    });
+  });
+
+// Define Routes for PUT and DELETE
+app
+  .route("/posts/:id")
+  .get((req, res, next) => {
+    const postId = req.params.id;
+
+    // Check if the post with given id exists
+    const postFound = posts.find((post) => {
+      return post.id === Number(postId);
+    });
+    if (!postFound) {
+      return res.status(404).json({
+        error: "No post found with the given id!",
+      });
+    }
+
+    res.status(202).json({
+      data: [postFound],
+      message: "The post data fetched successfully!",
+    });
+  })
+  .put((req, res, next) => {
+    const postId = req.params.id;
+    const { title, author, content } = req.body;
+
+    // Check if the post with given id exists
+    const postFound = posts.find((post) => {
+      return post.id === Number(postId);
+    });
+    if (!postFound) {
+      return res.status(404).json({
+        error: "No post found with the given id!",
+      });
+    }
+
+    // Update the post's data if given
+    if (title) postFound.title = title;
+    if (author) postFound.author = author;
+    if (content) postFound.content = content;
+
+    res.status(202).json({
+      data: [posts],
+      message: "The post data updated successfully!",
+    });
+  })
+  .delete((req, res, next) => {
+    const postId = req.params.id;
+
+    // Check if the post with given id exists
+    const postFoundIndex = posts.findIndex((post) => {
+      return post.id === Number(postId);
+    });
+    if (postFoundIndex < 0) {
+      return res.status(404).json({
+        error: "No post found with the given id!",
+      });
+    }
+
+    // Remove the post from the database
+    posts = posts.filter((post) => {
+      return post.id !== Number(postId);
+    });
+
+    res.status(202).json({
+      data: [posts],
+      message: "The post deleted successfully!",
+    });
+  });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`The server is listening on port ${PORT}`);
 });

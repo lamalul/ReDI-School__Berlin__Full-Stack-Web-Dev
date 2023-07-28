a6d6e9c6e5c16aa94104e9cfe4871f2c5e032999
mustafa-sarshar <mustafa.sarshar88@gmail.com>
2023-07-28 21:38:27 +0200

    add cors example

---
+++ Back-End/ExpressJS/07-cors.js
@@ -0,0 +1,35 @@
+const express = require("express"),
+  cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+// Apply cross-origin restriction to HTTP requests
+const whitelist = ["http://main-server.com"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log(origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// Define routes
+app
+  .route("/private-data") // Route: /private-data
+  .get(cors(corsOptions), (req, res, next) => {
+    res.send("You are allowed to get the private data!");
+  });
+
+// Define the main error handler
+app.use((err, req, res, next) => {
+  const errorCode = req.errorCode || 400;
+  res.status(errorCode).json({ error: err.message });
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is listening on port ${PORT}`);
+});
